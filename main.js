3c13d3cd167df3145b851230762c4756a06a4441
Jyotsna265 <74059430+Jyotsna265@users.noreply.github.com>
2021-08-24 19:02:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,9 +20,24 @@ var ball = {
     dx:3,
     dy:3
 }
+rightWristY = 0;
+rightWristX = 0;
+scoreRightWrist = 0;
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+
+video = createCapture(VIDEO);
+video.size(700, 600);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
 }
 
 
